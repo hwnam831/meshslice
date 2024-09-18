cbc92a145f672738d566edfb8cefae42c2a510e0
Hyoungwook Nam <hwnam831@gmail.com>
2024-09-18 16:41:26 -0500

    test cublas works well

--- gpu_simulator/TestCUBLAS.cu
+++ gpu_simulator/TestCUBLAS.cu
@@ -1,6 +1,6 @@
 #include <mpi.h>
-#include <nvshmem.h>
-#include <nvshmemx.h>
+//#include <nvshmem.h>
+//#include <nvshmemx.h>
 #include <algorithm>
 #include <cassert>
 #include <cmath>
@@ -16,57 +16,91 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
-#define CUDA_RT_CALL(call)                                                                  \
-    {                                                                                       \
-        cudaError_t cudaStatus = call;                                                      \
-        if (cudaSuccess != cudaStatus) {                                                    \
-            fprintf(stderr,                                                                 \
-                    "ERROR: CUDA RT call \"%s\" in line %d of file %s failed "              \
-                    "with "                                                                 \
-                    "%s (%d).\n",                                                           \
-                    #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus); \
-            exit( cudaStatus );                                                             \
-        }                                                                                   \
-    }
+#include "cuda_helper.h"
 
 half* initMatrix(size_t H, size_t W, int val){
-    //half* buf_h = (half *)malloc(sizeof(half) * H * W);
-    //for (int i=0; i<H*W; i++)
-    //    buf_h[i] = val;
+    half* buf_h = (half *)malloc(sizeof(half) * H * W);
+    for (int i=0; i<H*W; i++)
+        buf_h[i] = (half)val;
     half* buf_d = NULL;
     CUDA_RT_CALL(cudaMalloc(reinterpret_cast<void **>(&buf_d),
                              sizeof(half) * H * W));
-    //CUDA_RT_CALL(cudaMemcpy(buf_d, buf_h, sizeof(half) * H * W,
-    //                         cudaMemcpyHostToDevice));
-    CUDA_RT_CALL(cudaMemset(buf_d, val, sizeof(half) * H * W));
+    CUDA_RT_CALL(cudaMemcpy(buf_d, buf_h, sizeof(half) * H * W,
+                             cudaMemcpyHostToDevice));
+    free(buf_h);
     return buf_d;
 }
 
-#define M 1280
-#define N 512
-#define K 1024
+#define M 2560
+#define N 2048
+#define K 3072
+#define REPEAT 10
 
 int main(){
 
     half* A = initMatrix(M,K,1);
-    half* B = initMatrix(M,K,2);
+    half* B = initMatrix(N,K,2);
     half* C = initMatrix(M,N,0);
-    const float alpha = 1.0f;
-    const float beta = 0.0f;
+    const half alpha = 1.0f;
+    const half beta = 0.0f;
     cublasHandle_t handle;
     cudaEvent_t start, stop;
-
-    CUDA_RT_CALL(cublasCreate(&handle));
+    
+    checkCudaErrors(cublasCreate(&handle));
     // Perform warmup operation with cublas
-    CUDA_RT_CALL(cublasHgemm(
-        handle, CUBLAS_OP_N, CUBLAS_OP_N, N, MA,
+    cudaDeviceSynchronize();
+    checkCudaErrors(cublasHgemm(
+        handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M,
         K, &alpha, B, N, A, K,
         &beta, C, N));
+    cudaDeviceSynchronize();
+
+    // Allocate CUDA events that we'll use for timing
+    CUDA_RT_CALL(cudaEventCreate(&start));
+    CUDA_RT_CALL(cudaEventCreate(&stop));
+
+    // Record the start event
+    CUDA_RT_CALL(cudaEventRecord(start, NULL));
+
+    for (int j = 0; j < REPEAT; j++) {
+      // note cublas is column primary!
+      // need to transpose the order
+      checkCudaErrors(cublasHgemm(
+        handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M,
+        K, &alpha, B, N, A, K,
+        &beta, C, N));
+    }
+
+    printf("done.\n");
+
+    // Record the stop event
+    CUDA_RT_CALL(cudaEventRecord(stop, NULL));
+
+    // Wait for the stop event to complete
+    CUDA_RT_CALL(cudaEventSynchronize(stop));
+
+    float msecTotal = 0.0f;
+    CUDA_RT_CALL(cudaEventElapsedTime(&msecTotal, start, stop));
+
+    // Compute and print the performance
+    float msecPerMatrixMul = msecTotal / REPEAT;
+    double flopsPerMatrixMul = 2.0 * (double)M *
+                               (double)N *
+                               (double)K;
+    double gigaFlops =
+        (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
+    printf("Performance= %.2f GFlop/s, Time= %.3f msec, Size= %.0f Ops\n",
+           gigaFlops, msecPerMatrixMul, flopsPerMatrixMul);
+
     half* C_h = (half *)malloc(sizeof(half) * M * N);
     CUDA_RT_CALL(cudaMemcpy(C_h, C, sizeof(half) * M * N, cudaMemcpyDeviceToHost));
-    printf("C_h element %f\n", C_h[0]);
+
+    cudaDeviceSynchronize();
+    printf("C_h element %f\n", (float)C_h[0]);
+
     free(C_h);
-    CUDA_RT_CALL(cublasDestroy(handle));
+
+    checkCudaErrors(cublasDestroy(handle));
     CUDA_RT_CALL(cudaFree(A));
     CUDA_RT_CALL(cudaFree(B));
     return 0;
---
+++ gpu_simulator/cuda_helper.h
@@ -0,0 +1,89 @@
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+static const char *_cublasGetErrorEnum(cublasStatus_t error) {
+  switch (error) {
+    case CUBLAS_STATUS_SUCCESS:
+      return "CUBLAS_STATUS_SUCCESS";
+
+    case CUBLAS_STATUS_NOT_INITIALIZED:
+      return "CUBLAS_STATUS_NOT_INITIALIZED";
+
+    case CUBLAS_STATUS_ALLOC_FAILED:
+      return "CUBLAS_STATUS_ALLOC_FAILED";
+
+    case CUBLAS_STATUS_INVALID_VALUE:
+      return "CUBLAS_STATUS_INVALID_VALUE";
+
+    case CUBLAS_STATUS_ARCH_MISMATCH:
+      return "CUBLAS_STATUS_ARCH_MISMATCH";
+
+    case CUBLAS_STATUS_MAPPING_ERROR:
+      return "CUBLAS_STATUS_MAPPING_ERROR";
+
+    case CUBLAS_STATUS_EXECUTION_FAILED:
+      return "CUBLAS_STATUS_EXECUTION_FAILED";
+
+    case CUBLAS_STATUS_INTERNAL_ERROR:
+      return "CUBLAS_STATUS_INTERNAL_ERROR";
+
+    case CUBLAS_STATUS_NOT_SUPPORTED:
+      return "CUBLAS_STATUS_NOT_SUPPORTED";
+
+    case CUBLAS_STATUS_LICENSE_ERROR:
+      return "CUBLAS_STATUS_LICENSE_ERROR";
+  }
+
+  return "<unknown>";
+}
+
+
+template <typename T>
+void check(T result, char const *const func, const char *const file,
+           int const line) {
+  if (result) {
+    fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+            static_cast<unsigned int>(result), _cublasGetErrorEnum(result), func);
+    exit(EXIT_FAILURE);
+  }
+}
+
+// This will output the proper CUDA error strings in the event
+// that a CUDA host call returns an error
+#define checkCudaErrors(val) check((val), #val, __FILE__, __LINE__)
+
+
+#define MPI_CALL(call)                                                                \
+    {                                                                                 \
+        int mpi_status = call;                                                        \
+        if (MPI_SUCCESS != mpi_status) {                                              \
+            char mpi_error_string[MPI_MAX_ERROR_STRING];                              \
+            int mpi_error_string_length = 0;                                          \
+            MPI_Error_string(mpi_status, mpi_error_string, &mpi_error_string_length); \
+            if (NULL != mpi_error_string)                                             \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %s "                                                    \
+                        "(%d).\n",                                                    \
+                        #call, __LINE__, __FILE__, mpi_error_string, mpi_status);     \
+            else                                                                      \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %d.\n",                                                 \
+                        #call, __LINE__, __FILE__, mpi_status);                       \
+            exit( mpi_status );                                                       \
+        }                                                                             \
+    }
+
+#define CUDA_RT_CALL(call)                                                                  \
+    {                                                                                       \
+        cudaError_t cudaStatus = call;                                                      \
+        if (cudaSuccess != cudaStatus) {                                                    \
+            fprintf(stderr,                                                                 \
+                    "ERROR: CUDA RT call \"%s\" in line %d of file %s failed "              \
+                    "with "                                                                 \
+                    "%s (%d).\n",                                                           \
+                    #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus); \
+            exit( cudaStatus );                                                             \
+        }                                                                                   \
+    }
